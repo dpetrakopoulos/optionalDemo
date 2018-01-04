8fc892421055459861e54784972e9eaaee3172ac
Dionisis Petrakopoulos <dpetrakopoulos@tripsta.com>
2018-01-04 12:52:38 +0200

    First commit

---
+++ src/optional_demo/OptionalDemo.java
@@ -0,0 +1,28 @@
+package optional_demo;
+
+import java.util.Optional;
+
+public class OptionalDemo {
+
+	static void execute() {
+		Optional<String> maybeString = Optional.of("foo");
+		//to test for empty uncomment next line
+		//Optional<String> maybeString = Optional.empty();
+		
+		String newString = maybeString.map(s-> {
+				System.out.println("only run if optional is filled "); 
+				return s;
+			}
+		).orElseGet(() -> runIfEmpty());
+		System.out.println(newString);
+	}
+
+	private static String runIfEmpty() {
+		System.out.println("only run if empty");
+		return "empty";
+	}
+
+	public static void main(String[] args) {
+		execute();
+	}
+}
